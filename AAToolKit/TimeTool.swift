ff8eeedf804bb9c6c0a3e9c0ce58119dfd9d5951
Aaron Feng <516674612@qq.com>
2019-09-20 17:46:07 +0800

    时间转换工具优化

--- AAToolKit/TimeTool.swift
+++ AAToolKit/TimeTool.swift
@@ -13,14 +13,18 @@ public extension AAToolWrapper where Base == Double {
     func getTimeDescription() -> String {
         let forMatter = DateFormatter()
         let now = Date().timeIntervalSince1970
+        let pointDate = Date.init(timeIntervalSince1970: base)
         let duration = now - base
         forMatter.dateFormat = "D"
         let day = Int(forMatter.string(from: .init(timeIntervalSince1970: self.base))) ?? 0
         let nowDay = Int(forMatter.string(from: .init())) ?? 0
+        forMatter.dateFormat = "y"
+        let nowYear = forMatter.string(from: Date())
+        let pointYear = forMatter.string(from: pointDate)
         if duration < 0 {
             //未来
             forMatter.dateFormat = "yyyy-MM-dd HH:mm"
-            return forMatter.string(from: .init(timeIntervalSince1970: base))
+            return forMatter.string(from: pointDate)
         } else if duration < 60 * 2 {
             //两分钟之内
             return "刚刚"
@@ -30,25 +34,22 @@ public extension AAToolWrapper where Base == Double {
         } else if duration < 60 * 60 * 24 {
             //一天内
             return "\(Int(duration / 60 / 60))小时前"
-        } else if nowDay - day == 1 {
-            //昨天
-            return "昨天"
-        } else if nowDay - day == 2 {
-            //前天
-            return "前天"
-        } else if nowDay - day <= 7 {
-            //一周内
-            return "\(nowDay - day)天前"
-        } else {
-            forMatter.dateFormat = "y"
-            let pointDate = Date.init(timeIntervalSince1970: base)
-            let nowYear = forMatter.string(from: Date())
-            let pointYear = forMatter.string(from: pointDate)
-            if nowYear != pointYear {
-                forMatter.dateFormat = "y年M月d日"
+        } else if nowYear == pointYear {
+            if nowDay - day == 1 {
+                //昨天
+                return "昨天"
+            } else if nowDay - day == 2 {
+                //前天
+                return "前天"
+            } else if nowDay - day <= 7 {
+                //一周内
+                return "\(nowDay - day)天前"
             } else {
                 forMatter.dateFormat = "M月d日"
+                return forMatter.string(from: pointDate)
             }
+        } else {
+            forMatter.dateFormat = "y年M月d日"
             return forMatter.string(from: pointDate)
         }
     }
