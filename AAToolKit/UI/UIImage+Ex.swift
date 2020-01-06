36130310468b465cb460b6cbd0d09aa937481362
迪哥小跟班 <dixian2008@gmail.com>
2020-01-06 15:26:42 +0800

    feat:imageWithColor

--- AAToolKit/UI/UIImage+Ex.swift
+++ AAToolKit/UI/UIImage+Ex.swift
@@ -47,4 +47,17 @@ public extension UIImage {
         return image
     }
     
+    static func color(_ color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
+        let ctx = UIGraphicsGetCurrentContext()
+        ctx?.setFillColor(color.cgColor)
+        ctx?.fill(rect)
+        var image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        let imageData = image.jpegData(compressionQuality: 1.0)!
+        image = UIImage(data: imageData)!
+        return image
+    }
+    
 }
